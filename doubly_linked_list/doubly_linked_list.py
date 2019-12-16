39988cac174def1e377663c472972723136fae7c
BuildTools <arthurpisakhov1@gmail.com>
2019-12-16 13:52:05 -0500

    move_to

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -101,13 +101,18 @@ class DoublyLinkedList:
             return
 
         value = node.value
-        node.delete()
+        self.delete(node)
         self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return
+
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
