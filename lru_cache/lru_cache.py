909e2265495bc1dce0ab6800b63026ba6badcd4c
BuildTools <arthurpisakhov1@gmail.com>
2019-12-17 15:44:55 -0500

    LRU init

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +11,9 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.cache = {}
+        self.limit = limit
+        self.storage = DoublyLinkedList()
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +23,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # if key in self.cache:
+        #     self.dll(key)
+        #     return f'Cached...{self.cache[key]}'
+        
+        # if len(self.cache) == self.limit:
+
 
     """
     Adds the given key-value pair to the cache. The newly-
